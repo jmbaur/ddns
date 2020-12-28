e93983394f423375e27b0e4f7c610bf16e8b24f6
Jared Baur <jaredbaur@fastmail.com>
2020-12-28 03:44:14 +0000

    Added log messages if job fails

--- main.go
+++ main.go
@@ -18,9 +18,15 @@ type cfRecord struct {
 	Content  string `json:"content"`
 }
 
+type cfErr struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
 type cfResp struct {
-	Result  []cfRecord
-	Success bool `json:"success"`
+	Result  []cfRecord `json:"result"`
+	Success bool       `json:"success"`
+	Errors  []cfErr    `json:"errors"`
 }
 
 func main() {
@@ -39,7 +45,7 @@ func main() {
 		log.Fatal(err)
 	}
 	extIP := string(data)
-	/////////////////////////////////////////////////////////////////////////////
+
 	client := &http.Client{}
 	cfGet, err := http.NewRequest(http.MethodGet, fmt.Sprintf("https://api.cloudflare.com/client/v4/zones/%s/dns_records", zoneID), nil)
 	if err != nil {
@@ -62,14 +68,13 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// TODO: loop through results
+
 	var recordOfInterest cfRecord
 	for _, v := range cfRespData.Result {
 		if v.Name == recordName {
 			recordOfInterest = cfRespData.Result[0]
 		}
 	}
-	/////////////////////////////////////////////////////////////////////////////
 	if recordOfInterest.Content != extIP {
 		body, _ := json.Marshal(struct {
 			Content string `json:"content"`
@@ -82,9 +87,20 @@ func main() {
 		cfPatch.Header.Add("Content-Type", "application/json")
 		cfPatch.Header.Add("X-Auth-Email", email)
 		cfPatch.Header.Add("X-Auth-Key", apiToken)
-		_, err = client.Do(cfPatch)
+		resp, err = client.Do(cfPatch)
 		if err != nil {
 			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+		data, err := ioutil.ReadAll(resp.Body)
+		cfPatchResp := cfResp{}
+		json.Unmarshal(data, &cfPatchResp)
+		if cfPatchResp.Success != true {
+			log.Println("Failed to update external IP")
+			for _, v := range cfPatchResp.Errors {
+				log.Printf("code: %d message: %s\n", v.Code, v.Message)
+			}
+			os.Exit(1)
 		}
 		fmt.Println("External IP changed, updated Cloudflare")
 	} else {
